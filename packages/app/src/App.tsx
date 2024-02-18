868db1c04d948cfd19b95a1f6d1e7fcec7ee0cbe
hpungsan <hpung360@gmail.com>
2024-02-18 18:39:47 -0500

    basic ui and template added

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -33,6 +33,11 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { HomepageCompositionRoot } from '@backstage/plugin-home';
+import { HomePage } from './components/home/HomePage';
+import { createUnifiedTheme, PageTheme, shapes } from '@backstage/theme';
+
+
 
 const app = createApp({
   apis,
@@ -57,7 +62,9 @@ const app = createApp({
 
 const routes = (
   <FlatRoutes>
-    <Route path="/" element={<Navigate to="catalog" />} />
+    <Route path="/" element={<HomepageCompositionRoot />}>
+      <HomePage />
+    </Route>
     <Route path="/catalog" element={<CatalogIndexPage />} />
     <Route
       path="/catalog/:namespace/:kind/:name"
---
+++ packages/app/src/myUI.tsx
@@ -0,0 +1,51 @@
+import { BackstageOverrides } from '@backstage/core-components';
+import { UnifiedTheme } from '@backstage/theme';
+
+export const customThemeOverrides = (
+  theme: UnifiedTheme,
+): BackstageOverrides => {
+  return {
+    BackstagePage: {
+      root: {
+        display: 'grid',
+        overflow: 'hidden !important',
+        gridTemplateColums: '30px 280px 3fr 30px',
+        gridTemplateAreas:
+          '"header header header header" ". sidenav content ." ". sidenav content . "',
+      },
+    },
+
+    BackstageHeader: {
+      header: {
+        height: '80px',
+        gridArea: 'header',
+        display: 'flex',
+        borderBottom: `1px solid #FFFFFF`,
+        backgroundImage: `none`,
+        backgroundPosition: 'center',
+        padding: '50px',
+        boxShadow: 'none',
+        background: `#FFFFFF`
+      },
+
+      title: {
+        color: `#FFFFFF`,
+        fontSize: 24,
+      },
+      subtitle: {
+        color: `#666666`,
+      },
+      type: {
+        color: `#FFFFFF`,
+      },
+    },
+    BackstageHeaderLabel: {
+      label: {
+        color: '#FFFFFF',
+      },
+      root: { color: '#FFFFFF',  },
+      value: { color: '#FFFFFF' },
+    },
+
+  };
+};
